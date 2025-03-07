276473f6b4cf299ddbd3847c7e300053a26497c5
mybagfull <baezgamer01@gmail.com>
2025-03-07 15:39:09 +0300

    Indicator animation.

--- app/src/main/java/com/denishrynkevich/onboardingcompose/onboard/IndicatorUI.kt
+++ app/src/main/java/com/denishrynkevich/onboardingcompose/onboard/IndicatorUI.kt
@@ -1,15 +1,11 @@
 package com.denishrynkevich.onboardingcompose.onboard
 
+import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -23,53 +19,44 @@ fun IndicatorUI(
     selectedColor: Color = Color.White,
     unselectedColor: Color = Color.White.copy(alpha = 0.4f),
 ) {
-        Row(horizontalArrangement = Arrangement.SpaceBetween) {
-            repeat(pageSize) {
-                Box(
-                    modifier = Modifier
-                        .height(8.dp)
-                        .width(width = if (it == currentPage) 24.dp else 8.dp)
-                        .clip(RoundedCornerShape(20.dp))
-                        .background(color = if (it == currentPage) selectedColor else unselectedColor)
-
-                )
-                Spacer(modifier = Modifier.size(if(it == 3) 0.dp else 8.dp))
-
-            }
-
+    Row(horizontalArrangement = Arrangement.SpaceBetween) {
+        repeat(pageSize) { index ->
+            val width by animateDpAsState(
+                targetValue = if (index == currentPage) 24.dp else 8.dp,
+                label = "indicatorWidthAnimation"
+            )
+            Box(
+                modifier = Modifier
+                    .height(8.dp)
+                    .width(width)
+                    .clip(RoundedCornerShape(20.dp))
+                    .background(color = if (index == currentPage) selectedColor else unselectedColor)
+            )
+            Spacer(modifier = Modifier.size(if (index == pageSize - 1) 0.dp else 8.dp))
         }
-
+    }
 }
-
 
 @Preview(showBackground = false)
 @Composable
 fun IndicatorUIPreview1() {
-
     IndicatorUI(pageSize = 4, currentPage = 0)
-
 }
 
 @Preview(showBackground = false)
 @Composable
 fun IndicatorUIPreview2() {
-
-        IndicatorUI(pageSize = 4, currentPage = 1)
-
+    IndicatorUI(pageSize = 4, currentPage = 1)
 }
 
 @Preview(showBackground = false)
 @Composable
 fun IndicatorUIPreview3() {
-
-        IndicatorUI(pageSize = 4, currentPage = 2)
-
+    IndicatorUI(pageSize = 4, currentPage = 2)
 }
 
 @Preview(showBackground = false)
 @Composable
 fun IndicatorUIPreview4() {
-
-        IndicatorUI(pageSize = 4, currentPage = 3)
-
+    IndicatorUI(pageSize = 4, currentPage = 3)
 }
